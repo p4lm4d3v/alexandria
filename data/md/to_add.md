9d0cfc75c32e8af26028cea03091e98bf6b812e2
p4lm4d3v <p4lm4d3v@gmail.com>
2024-06-19 18:11:21 +0200

    removed flutter project; it does not work for some reason?

--- data/md/to_add.md
+++
@@ -1,1 +0,0 @@
-[10 Best Movies to Boost you Mood (Feel Good Movies)](https://www.instagram.com/reel/C5ETw0lC-rU/?igsh=MTR0YThtaHZpanZycQ%3D%3D)
--- lib/functions/get_current_page_title.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Text getCurrentPageTitle(context) {
-  String title = 'Not Implemented';
-  return Text(
-    title,
-    textAlign: TextAlign.center,
-  );
-}
